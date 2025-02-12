e0ed5117bb55540ae1552b51e70c90da740d38d1
Zak Goneau <zak.goneau@stu.ocsb.ca>
2025-02-12 16:04:45 +0000

    Started Swift Intro-06

---
+++ VolSphere.swift
@@ -0,0 +1,35 @@
+//
+//  VolSphere.swift
+//
+//  Created by Zak Goneau
+//  Created on 2025-02-12
+//  Version 1.0
+//  Copyright (c) 2025 Zak Goneau. All rights reserved.
+//
+//  Volume of sphere calculator in swift
+
+// Import libraries
+import Foundation
+
+// Introduce program
+print("This program calculates the volume of a sphere.")
+
+// Get user input
+print("Please enter the radius (cm): .")
+let radius = readLine()
+
+// Cast radius to float
+let radiusFloat = (radius, !as! NSString)!floatValue
+
+// Check if number is positive
+if (radiusFloat >= 0 ) {
+    // Calculate volume
+    let volume = (3.0/4.0) * (Double.pi*pow(radiusFloat, 3.0))
+
+    // Print volume
+    print(volume)
+
+// Tell user number is negative
+} else {
+    print("No negative numbers allowed.")
+}
